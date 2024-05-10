ec4382ebc57ace7c1408104770b3fa31b4e687ec
Elva <elvachen2000@gmail.com>
2024-05-10 15:38:52 +0800

    Split the code in function files and header files

---
+++ include/functions.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTIONS_H
+#define FUNTCIONS_H
+
+// Function declarations for On_And_Off_Button code
+
+void printTouchToSerial(int touchX, int touchY, int touchZ);
+void drawFrame();
+void drawRedButton();
+void drawGreenButton();
+
+// End declaration
+
+#endif // FUNCTIONS_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,84 +1,44 @@
 #include <SPI.h>
-#include <TFT_eSPI.h> // See README.txt for detailed configeration made for libdeps/TFT_eSPI/User_Setup.h
-#include <Arduino.h>
+#include <TFT_eSPI.h>
 #include <XPT2046_Touchscreen.h>
 
-TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
+#include "config.h"
+#include "functions.h"
 
-// Touchscreen pinout
-#define XPT2046_IRQ 36
-#define XPT2046_MOSI 32
-#define XPT2046_MISO 39
-#define XPT2046_CLK 25
-#define XPT2046_CS 33
+TFT_eSPI tft = TFT_eSPI();
 
 SPIClass touchscreenSPI = SPIClass(VSPI);
 XPT2046_Touchscreen touchscreen(XPT2046_CS, XPT2046_IRQ);
 
-#define SCREEN_WIDTH 320
-#define SCREEN_HEIGHT 240
-#define FONT_SIZE 2
+// Touchscreen coordinates: (x, y) and pressure (z)
+int x, y, z;
 
-int x, y, z; // Touchscreen coordinates: (x, y) and pressure (z)
+// Stores current button state
+bool buttonState = false;
 
-// Print touchscreen info about X, Y and Pressure Z on the serial monitor
-void printTouchToSerial(int touchX, int touchY, int touchZ){
-  Serial.print("X = ");
-  Serial.print(touchX);
-  Serial.print(" | Y = ");
-  Serial.print(touchY);
-  Serial.print(" | Pressure = ");
-  Serial.print(touchZ);
-  Serial.println();
-}
-
-// Print touchscreen info about X, Y, and Pressure Z on the TFT display
-void printTouchToDisplay(int touchX, int touchY, int touchZ){
-  // Clear TFT screen
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-
-  int centerX = SCREEN_WIDTH / 2;
-  int textY = 80;
-
-  String tempText = "X = " + String(touchX);
-  tft.drawCentreString(tempText, centerX, textY, FONT_SIZE);
-
-  textY += 20;
-  tempText = "Y = " + String(touchY);
-  tft.drawCentreString(tempText, centerX, textY, FONT_SIZE);
-
-  textY += 20;
-  tempText = "Pressure = " + String(touchZ);
-  tft.drawCentreString(tempText, centerX, textY, FONT_SIZE);
-}
-
-void setup(){
+void setup() {
   Serial.begin(115200);
 
   // Start the SPI for the touchscreen and init the touchscreen
   touchscreenSPI.begin(XPT2046_CLK, XPT2046_MISO, XPT2046_MOSI, XPT2046_CS);
   touchscreen.begin(touchscreenSPI);
-
-  // Set the touchscreen rotation in landscape mode
-  // If the touchscreen is upside down, if so, set the rotation to 3: touchscreen.setRotation(3)
+  // Set the Touchscreen rotation in landscape mode
+  // Note: in some displays, the touchscreen might be upside down, so you might need to set the rotation to 3: touchscreen.setRotation(3);
   touchscreen.setRotation(1);
 
   // Start the tft display
   tft.init();
-  // Set the tft display rotation in landscape mode
+  // Set the TFT display rotation in landscape mode
   tft.setRotation(1);
 
   // Clear the screen before writing to it
-  tft.fillScreen(TFT_WHITE);
-  tft.setTextColor(TFT_BLACK, TFT_WHITE);
-  
-  // Set X and Y coordinates for center of display
-  int centerX = SCREEN_WIDTH / 2;
-  int centerY = SCREEN_HEIGHT / 2;
+  tft.fillScreen(TFT_BLACK);
 
-  tft.drawCentreString("Hello, world!", centerX, 30, FONT_SIZE);
-  tft.drawCentreString("Touch screen to test", centerX, centerY, FONT_SIZE);
+  // Draw button 
+  drawGreenButton();
+
+  pinMode(CYD_LED_GREEN, OUTPUT);
+  digitalWrite(CYD_LED_GREEN, LOW);
 }
 
 void loop() {
@@ -92,28 +52,26 @@ void loop() {
     z = p.z;
 
     printTouchToSerial(x, y, z);
-    printTouchToDisplay(x, y, z);
 
-    delay(100);
+    if (buttonState) {
+      Serial.println("ON");
+      if ((x > REDBUTTON_X) && (x < (REDBUTTON_X + REDBUTTON_W))) {
+        if ((y > (REDBUTTON_Y)) && (y <= (REDBUTTON_Y + REDBUTTON_H))) {
+          Serial.println("Red button pressed");
+          drawRedButton();
+          digitalWrite(CYD_LED_GREEN, HIGH);
+        }
+      }
+    }
+    else {
+      Serial.println("OFF");
+      if ((x > (GREENBUTTON_X)) && (x < (GREENBUTTON_X + GREENBUTTON_W))) {
+        if ((y > (GREENBUTTON_Y)) && (y <= (GREENBUTTON_Y + GREENBUTTON_H))) {
+          Serial.println("Green button pressed");
+          drawGreenButton();
+          digitalWrite(CYD_LED_GREEN, LOW);
+        }
+      }
+    }
   }
 }
-
-
-// #include <Arduino.h>
-
-// // put function declarations here:
-// int myFunction(int, int);
-
-// void setup() {
-//   // put your setup code here, to run once:
-//   int result = myFunction(2, 3);
-// }
-
-// void loop() {
-//   // put your main code here, to run repeatedly:
-// }
-
-// // put function definitions here:
-// int myFunction(int x, int y) {
-//   return x + y;
-// }
